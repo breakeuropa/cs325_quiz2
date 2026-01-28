d160422079d1b3119dacc6140dc4ebac2b84a001
breakeuropa <johansc@siue.edu>
2026-01-28 12:09:59 -0600

    Added multiplication and division functions

--- quiz2/calculator.py
+++ quiz2/calculator.py
@@ -4,10 +4,22 @@ def add(a: int, b: int) -> int:
     return sum
 
 def subtract(a: int, b: int) -> int:
-    difference = a + b
+    difference = a - b
     print(f"{a} - {b} = ", end = " ")
     return difference
+
+def mul(a: int, b: int) -> int:
+    product = a * b
+    print(f"{a} * {b} = ", end = " ")
+    return product
+
+def div(a: int, b: int) -> int:
+    quotient = a / b
+    print(f"{a} / {b} = ", end = " ")
+    return quotient
 
 if __name__ == "__main__":
     print(add(1, 2))
     print(subtract(5, 3))
+    print(mul(2, 2))
+    print(div(15, 3))
